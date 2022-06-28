0b504cff24d3e8275566500fd83b9dd93fe48525
Alex <lunoface@mail.ru>
2022-06-28 21:21:50 +0700

    create dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -3,12 +3,12 @@ import { IsEmail, IsString, Length } from "class-validator";
 
 
 export class CreateUserDto {
-  @ApiProperty({example: 'user@mail.ru', description: 'mail'})
-  @IsString({message: 'to be string'})
-  @IsEmail({},{message: 'uncorrect email'})
-  readonly email: string
-  @ApiProperty({example: '1234', description: 'pass'})
-  @IsString({message: 'to be string'})
-  @Length(4, 16, {message: 'gt 4, lt: 16'})
-  readonly password: string
+    @ApiProperty({ example: "user@mail.ru", description: "mail" })
+    @IsString({ message: "to be string" })
+    @IsEmail({}, { message: "uncorrect email" })
+    readonly email: string;
+    @ApiProperty({ example: "1234", description: "pass" })
+    @IsString({ message: "to be string" })
+    @Length(4, 16, { message: "gt 4, lt: 16" })
+    readonly password: string;
 }
