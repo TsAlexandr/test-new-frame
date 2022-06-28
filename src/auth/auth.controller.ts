0469ca7107b6f8a5b0040d0b9a85eb8958740643
Alex <lunoface@mail.ru>
2022-06-28 21:13:59 +0700

    authorization

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,21 +3,21 @@ import { ApiTags } from "@nestjs/swagger";
 import { CreateUserDto } from "../users/dto/create-user.dto";
 import { AuthService } from "./auth.service";
 
-@ApiTags('Authorization')
-@Controller('auth')
+@ApiTags("Authorization")
+@Controller("auth")
 export class AuthController {
 
 
-  constructor(private authService: AuthService) {
-  }
+    constructor(private authService: AuthService) {
+    }
 
-  @Post('/login')
-  login(@Body() userDto: CreateUserDto) {
-    return this.authService.login(userDto)
-  }
+    @Post("/login")
+    login(@Body() userDto: CreateUserDto) {
+        return this.authService.login(userDto);
+    }
 
-  @Post('/registration')
-  registration(@Body() userDto: CreateUserDto) {
-    return this.authService.registration(userDto)
-  }
+    @Post("/registration")
+    registration(@Body() userDto: CreateUserDto) {
+        return this.authService.registration(userDto);
+    }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,38 +8,38 @@ import { User } from "../users/users-model";
 @Injectable()
 export class AuthService {
 
-  constructor(private userService: UsersService,
-              private jwtService: JwtService) {
-  }
-
-  async login(userDto: CreateUserDto) {
-    const user = await this.validateUser(userDto);
-    return this.generateToken(user);
-  }
-
-  async registration(userDto: CreateUserDto) {
-    const candidate = await this.userService.getUserByEmail(userDto.email);
-    if (candidate) {
-      throw new HttpException("User is already in use", HttpStatus.BAD_REQUEST);
+    constructor(private userService: UsersService,
+                private jwtService: JwtService) {
     }
-    const hashPassword = await bcrypt.hash(userDto.password, 5);
-    const user = await this.userService.createUser({ ...userDto, password: hashPassword });
-    return this.generateToken(user);
-  }
 
-  async generateToken(user: User) {
-    const payload = { email: user.email, id: user.id, roles: user.roles };
-    return {
-      token: this.jwtService.sign(payload)
-    };
-  }
-
-  private async validateUser(userDto: CreateUserDto) {
-    const user = await this.userService.getUserByEmail(userDto.email);
-    const passwordEquals = await bcrypt.compare(userDto.password, user.password);
-    if (user && passwordEquals) {
-      return user;
+    async login(userDto: CreateUserDto) {
+        const user = await this.validateUser(userDto);
+        return this.generateToken(user);
     }
-    throw new UnauthorizedException({ message: "Invalid email or password" });
-  }
+
+    async registration(userDto: CreateUserDto) {
+        const candidate = await this.userService.getUserByEmail(userDto.email);
+        if (candidate) {
+            throw new HttpException("User is already in use", HttpStatus.BAD_REQUEST);
+        }
+        const hashPassword = await bcrypt.hash(userDto.password, 5);
+        const user = await this.userService.createUser({ ...userDto, password: hashPassword });
+        return this.generateToken(user);
+    }
+
+    async generateToken(user: User) {
+        const payload = { email: user.email, id: user.id, roles: user.roles };
+        return {
+            token: this.jwtService.sign(payload)
+        };
+    }
+
+    private async validateUser(userDto: CreateUserDto) {
+        const user = await this.userService.getUserByEmail(userDto.email);
+        const passwordEquals = await bcrypt.compare(userDto.password, user.password);
+        if (user && passwordEquals) {
+            return user;
+        }
+        throw new UnauthorizedException({ message: "Invalid email or password" });
+    }
 }
