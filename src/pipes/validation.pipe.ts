b333e11abf7673b31d3eb924a1512974ef54d24d
Alex <lunoface@mail.ru>
2022-06-28 21:24:50 +0700

    use jot, validation

--- src/pipes/validation.pipe.ts
+++ src/pipes/validation.pipe.ts
@@ -4,18 +4,18 @@ import { validate } from "class-validator";
 import { ValidationException } from "../exceptions/validation.exception";
 
 @Injectable()
-export class ValidationPipe implements PipeTransform<any>{
-  async transform(value: any, metadata: ArgumentMetadata): Promise<any> {
-    const obj = plainToInstance(metadata.metatype, value)
-    const error = await validate(obj)
-    if(error.length) {
-      let messages = error.map(err => {
-        return `${err.property} - ${Object.values(err.constraints).join(", ")}`
-      })
-      throw new ValidationException(messages)
-    }
-    return value
+export class ValidationPipe implements PipeTransform<any> {
+    async transform(value: any, metadata: ArgumentMetadata): Promise<any> {
+        const obj = plainToInstance(metadata.metatype, value);
+        const error = await validate(obj);
+        if (error.length) {
+            let messages = error.map(err => {
+                return `${err.property} - ${Object.values(err.constraints).join(", ")}`;
+            });
+            throw new ValidationException(messages);
+        }
+        return value;
 
-  }
+    }
 
 }
