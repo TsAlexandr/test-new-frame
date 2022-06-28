5c88adb51cd8179b16ee577027a2ca8695f90678
Alex <lunoface@mail.ru>
2022-06-28 21:23:19 +0700

    create module

--- src/app-module.ts
+++ src/app-module.ts
@@ -5,26 +5,33 @@ import { ConfigModule } from "@nestjs/config";
 import { User } from "./users/users-model";
 import { Role } from "./roles/roles-model";
 import { UserRoles } from "./roles/user-roles.model";
+import { Post } from "./posts/posts.model";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import * as path from 'path'
 
 
 @Module({
     controllers: [],
     providers: [],
     imports: [
-      ConfigModule.forRoot({
-          envFilePath: `.${process.env.NODE_ENV}.env`
-      }),
+        ConfigModule.forRoot({
+            envFilePath: `.${process.env.NODE_ENV}.env`
+        }),
+        ServeStaticModule.forRoot({
+           rootPath: path.resolve(__dirname, 'static')
+        }),
         SequelizeModule.forRoot({
-            dialect: 'postgres',
+            dialect: "postgres",
             host: process.env.POSTGRES_HOST,
             port: Number(process.env.POSTGRES_PORT),
             username: process.env.POSTGRES_USER,
             password: process.env.POSTGRES_PASSWORD,
             database: process.env.POSTGRES_DB,
-            models: [User, Role, UserRoles],
+            models: [User, Role, UserRoles, Post],
             autoLoadModels: true
         }),
-      UsersModule,
+        UsersModule
     ]
 })
-export class AppModule {}
+export class AppModule {
+}
